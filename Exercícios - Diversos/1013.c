59fbd986fb76add17c1531b765154544f32e69b5
Ana Luisa Rodrigues <analurodriza@gmail.com>
2024-07-22 11:31:53 -0300

    1012 a 1037 anunciado

--- Exercícios - Diversos/1013.c
+++ Exercícios - Diversos/1013.c
@@ -1,4 +1,11 @@
 /* PROBLEMA 1013
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. 
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
 */
 
 #include <stdio.h>
